4b9e971d49a9bfac5482423a75da7f4cd0986dc2
Kris C <kris541@hotmail.com>
2014-09-18 09:39:23 -0500

    updated db to codeup_pdo_test_db

--- public/php_pdo.php
+++ public/php_pdo.php
@@ -1,6 +1,6 @@
 <?php
 // Get new instance of PDO object
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_test_db', 'codeup', 'codeuprocks');
+$dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'codeup', 'codeuprocks');
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
