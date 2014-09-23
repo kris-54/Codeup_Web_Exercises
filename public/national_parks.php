d510f76bf45fb1346639eb86decb04e60ec73f48
Kris C <kris541@hotmail.com>
2014-09-23 14:23:12 -0500

    added prepared statments and fixed spacing

--- public/national_parks.php
+++ public/national_parks.php
@@ -11,16 +11,38 @@ require '../dbc.php';
 // You should accept one or two parameters from $_GET and use them to load the appropriate parks.
 $offset = isset($_GET['offset']) ? intval($_GET['offset']) : 0;
 
-
-
-$stmt = $dbc->query("SELECT name, location, date_established, area_in_acres 
-        FROM national_parks LIMIT 4 OFFSET $offset");
-
-$parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+$stmt = $dbc->prepare("SELECT name, location, area_in_acres, date_established, description FROM national_parks LIMIT :num OFFSET :offset");
+$stmt->bindValue(':num', 4, PDO::PARAM_INT);
+$stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+$stmt->execute();
+$parks = $stmt->fetchALL(PDO::FETCH_ASSOC);
 
 $count = $dbc->query('SELECT count(*) FROM national_parks');
 $number = $count->fetchColumn();
+
+$valid = FALSE;
+if (isset($_POST)) {
+    foreach ($_POST as $value ) {
+        if (empty($value)) {
+            $valid = FALSE;
+            break;
+        }
+        else {
+            $valid = TRUE;
+        }
+    }
+}
+
+if($valid) {
+    $stmt = $dbc->prepare('INSERT INTO national_parks (name, location, area_in_acres, date_established, description) VALUES (:name, :location, :area_in_acres, :date_established, :description)');
+    $stmt->bindValue(':name', $_POST['name'], PDO::PARAM_STR);
+    $stmt->bindValue(':location', $_POST['location'], PDO::PARAM_STR);
+    $stmt->bindValue(':area_in_acres', $_POST['area_in_acres'], PDO::PARAM_STR);
+    $stmt->bindValue(':date_established', $_POST['date_established'], PDO::PARAM_STR);
+    $stmt->bindValue(':description', $_POST['description'], PDO::PARAM_STR);
+    $stmt->execute();
+}
+
 
 
 
@@ -51,6 +73,7 @@ $number = $count->fetchColumn();
             <th>Location</th>
             <th>Date Established</th>
             <th>Acres</th>
+            <th>Description</th>
         </tr>
 
         <? foreach ($parks as $park) : ?>
@@ -72,7 +95,32 @@ $number = $count->fetchColumn();
         <?php endif;?>
     </ul>
 
-    
+    <form method='POST'action='national_parks.php'>
+         <p>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" placeholder="Add Here">
+        
+
+            <label for="location">Location</label>
+            <input type="text" id="location" name="location" placeholder="Add Here">
+
+            <label for="date_established">Date Established</label>
+            <input type="text" id="date_established" name="date_established" placeholder="Add Here">
+
+            <label for="area_in_acres">Acres</label>
+            <input type="text" id="area_in_acres" name="area_in_acres" placeholder="Add Here">
+
+            <label for="description">Description</label>
+            <textarea name="description" id='description' placeholder='description'></textarea>
+
+
+
+            <input type="submit" value="Add">
+        </p>
+
+
+
+    </form>
 	    
 
 
